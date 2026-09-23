e85c4184dac0ff4da10c3bd3b184974f7da7831a
SKattendance <yipiaip@secondkill.com.hk>
2026-09-23 15:41:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "checkin-pwa-v4";
+const CACHE_NAME = "checkin-pwa-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(event){
